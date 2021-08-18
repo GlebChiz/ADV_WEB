c15f8503527ed4ea0c603a344fcc630b35dec1a9
VitaliyPlakhotnyk <v.plakhotnyk@urancompany.com>
2021-08-18 17:26:43 +0300

    feat: add crud for supervisor

--- src/app/screens/home/screens/supervisor-license/supervisor-license-table/supervisor-license-popup/supervisor-license-popup.component.ts
+++ src/app/screens/home/screens/supervisor-license/supervisor-license-table/supervisor-license-popup/supervisor-license-popup.component.ts
@@ -35,16 +35,14 @@ export class SupervisorLicensePopupComponent extends UnSubscriber implements OnI
 
 	public initForm(): void {
 		console.log('this.supervisorLicense', this.supervisorLicense);
-		// 	id: guid | null;
-		// clinicianId: guid;
-		// payerId: guid;
-		// start: datetime;
-		// end: datetime | null;
-		// providerId: string;
-		// clinicianName: string;
-
 		this.mySupervisorLicenseForm = new FormGroup({
 			id: new FormControl(this.supervisorLicense?.id || ''),
+			clinicianId: new FormControl(this.supervisorLicense?.clinicianId || ''),
+			payerId: new FormControl(this.supervisorLicense?.payerId || ''),
+			start: new FormControl(this.supervisorLicense?.start || ''),
+			end: new FormControl(this.supervisorLicense?.end || ''),
+			providerId: new FormControl(this.supervisorLicense?.providerId || ''),
+			clinicianName: new FormControl(this.supervisorLicense?.clinicianName || ''),
 		});
 	}
 
