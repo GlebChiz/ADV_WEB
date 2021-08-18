d6275d46171c2c522dab60cfedb8ee4041059741
dev <dev@devs-iMac.local>
2021-08-18 14:46:01 +0300

    feature: added buttons 'create', added CRUD opeartions on the public-snipit

--- src/app/screens/home/screens/public-snipit/public-snipit-table/public-snipit-table.component.ts
+++ src/app/screens/home/screens/public-snipit/public-snipit-table/public-snipit-table.component.ts
@@ -1,23 +1,30 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Component, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
+import { DialogCloseResult, DialogRef, DialogService } from '@progress/kendo-angular-dialog';
 import { CellClickEvent } from '@progress/kendo-angular-grid';
 import { CustomTableDirective } from 'src/app/shared/table/table.directive';
 import {
+	CREATE_ITEM_TABLE_PENDING,
 	DELETE_ITEM_TABLE_PENDING,
 	EDIT_ITEM_TABLE_PENDING,
 	GET_CURRENT_ITEM_PENDING,
 	GET_TABLE_DATA_PENDING,
 } from 'src/app/shared/table/table.tokens';
 import { IColumn } from '../../../../../shared/interfaces/column.interface';
+import { PublicSnipitPopupComponent } from './public-snipit-popup/public-snipit-popup.component';
 
 @Component({
 	providers: [],
 	selector: 'advenium-public-snipit-table',
 	templateUrl: './public-snipit-table.component.html',
+	styleUrls: ['../../../home.component.scss'],
 })
 export class PublicSnipitTableComponent extends CustomTableDirective {
 	public constructor(
+		// private dialogService: DialogService,
+		private dialogService: DialogService,
 		private _router: Router,
 		_store: Store<any>,
 		@Inject(GET_TABLE_DATA_PENDING) getTableDataPending: any,
@@ -25,8 +32,46 @@ export class PublicSnipitTableComponent extends CustomTableDirective {
 		// @Inject(CREATE_ITEM_TABLE_PENDING) private createDataPending: any,
 		@Inject(DELETE_ITEM_TABLE_PENDING) deleteDataPending: any,
 		@Inject(EDIT_ITEM_TABLE_PENDING) editDataPending: any,
+		@Inject(CREATE_ITEM_TABLE_PENDING) private createDataPending: any, // @Inject(CLEAR_CURRENT_ITEM) private clearCurrentItem: any,
 	) {
 		super(_store, getTableDataPending, getCurrentItemPending, deleteDataPending, editDataPending);
+	}
+
+	public deleteWithPopup(id: string): void {
+		if (!window.confirm(`Are you sure you want to delete ${this.controller}?`)) {
+			return;
+		}
+		this.delete(id);
+	}
+
+	public openDialog(dataItem?: any, isDublicate?: boolean): void {
+		if (dataItem) {
+			this._store.dispatch(
+				this.getCurrentItemPending({ id: dataItem.id, controller: this.controller }),
+			);
+		}
+		const dialog: DialogRef = this.dialogService.open({
+			title: 'Public Snipit',
+			content: PublicSnipitPopupComponent,
+			width: 600,
+			height: 310,
+			minWidth: 250,
+		});
+
+		dialog.content.instance.modality = { ...dataItem };
+		dialog.result.subscribe((result: any) => {
+			if (!(result instanceof DialogCloseResult)) {
+				if (isDublicate) {
+					result.id = null;
+				}
+				if (dataItem && !isDublicate) {
+					this._store.dispatch(this.editDataPending({ item: result, controller: this.controller }));
+					return;
+				}
+				this._store.dispatch(this.createDataPending({ item: result, controller: this.controller }));
+			}
+			// this._store.dispatch(this.clearCurrentItem());
+		});
 	}
 
 	public columns: IColumn[] = [
