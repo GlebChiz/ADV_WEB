7358a0f469f2660007d9b1b9681ecb82e8ab12fe
GlebChiz <gchyz@jsdaddy.io>
2021-09-02 13:34:41 +0300

    feature: added update demographic info and geting current person

---
+++ src/app/shared/interfaces/person.interface.ts
@@ -0,0 +1,6 @@
+import { IPersonDemographicInfo } from '../components/demografic/demographic.component';
+
+export interface IPersonState {
+	// eslint-disable-next-line @typescript-eslint/ban-types
+	personDemographicInfo: IPersonDemographicInfo | {};
+}
